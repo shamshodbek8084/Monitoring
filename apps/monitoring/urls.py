3e119607a4d5c97bac1442f97a23a640a90f0911
shamshodbek8084 <erkinmulkamonov@gmail.com>
2025-01-29 22:11:06 +0500

    View and urls for Project

--- apps/monitoring/urls.py
+++ apps/monitoring/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProjectAPIView
+# from .views import ProjectAPIView
+from .views import (Create_project,
+                    Read_project,
+                    List_project,
+                    Update_project,
+                    Delete_project)
 
 urlpatterns = [
-    path('project/<int:pk>', ProjectAPIView.as_view(), name = 'project'),
+    # path('project/<int:pk>', ProjectAPIView.as_view(), name = 'project'),
+    path('create_project/<int:pk>', Create_project.as_view(), name='create_project'),
+    path('read_project/<int:pk>', Read_project.as_view(), name='read_project'),
+    path('list_project/<int:pk>', List_project.as_view(), name='list_project'),
+    path('update_project/<int:pk>', Update_project.as_view(), name='update_project'),
+    path('delete_project/<int:pk>', Delete_project.as_view(), name='delete_project'),
 
 ]
--- apps/monitoring/views.py
+++ apps/monitoring/views.py
@@ -4,30 +4,65 @@ from .models import Payment, Project
 from rest_framework.response import Response
 from rest_framework.views import APIView 
 from rest_framework.permissions import AllowAny
+from rest_framework.generics import (CreateAPIView,
+                                        UpdateAPIView,
+                                        DestroyAPIView,
+                                        RetrieveAPIView,
+                                        ListAPIView)
 
 
 # Create your views here.
 
-class ProjectAPIView(APIView):
+
+# Class APIView bo'yicha qilindi-------------------------------------
+# class ProjectAPIView(APIView):
+#     serializer_class = ProjectSerializer
+#     permission_classes = (AllowAny, )
+#     model = Project
+#     def get(self, request, pk):
+#         project = get_object_or_404(self.model, id=pk)
+#         serializer = self.serializer_class(instance = project)
+#         data = {
+#             'data':serializer.data
+#         }
+#         return Response(data=data)
+    
+#     def put(self, request, pk):
+#         project = get_object_or_404(self.model, id=pk)
+#         serializer = self.serializer_class(instance = project)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.data)
+    
+        
+class Create_project(CreateAPIView):
+    queryset = Project.objects.filter(is_active = True)
+    serializer = ProjectSerializer
+    permission_classes = (AllowAny, )
+
+class Read_project(RetrieveAPIView):
+    queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = (AllowAny, )
-    model = Project
-    def get(self, request, pk):
-        project = get_object_or_404(self.model, id=pk)
-        serializer = self.serializer_class(instance = project)
-        data = {
-            'data':serializer.data
-        }
-        return Response(data=data)
-    
-    def put(self, request, pk):
-        project = get_object_or_404(self.model, id=pk)
-        serializer = self.serializer_class(instance = project)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.data)
-        
+
+class List_project(ListAPIView):
+    queryset = Project.objects.all()
+    serializer = ProjectSerializer
+    permission_classes = (AllowAny, )
+
+class Update_project(UpdateAPIView):
+    queryset = Project.objects.all()
+    serializer = ProjectSerializer
+    permission_classes = (AllowAny, )
+
+class Delete_project(DestroyAPIView):
+    queryset = Project.objects.filter(is_active = True)
+    serializer = ProjectSerializer
+    permission_classes = (AllowAny, )
+
+
+
 
 
